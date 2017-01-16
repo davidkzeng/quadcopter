55c7f9e32c244a615d12ff84af2a8217481beecd
David Zeng <davidzeng.42@gmail.com>
2017-01-16 16:27:07 -0500

    lol

--- quadcopter/quadcopter.ino
+++ quadcopter/quadcopter.ino
@@ -1,7 +1,23 @@
+#include <Servo.h>
+
+#define PIN_MOTOR0 9
+
+Servo motor0;
+
+int value = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  motor0.attach(PIN_MOTOR0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+  Serial.println("lol2");
+  motor0.writeMicroseconds(2000);
+  /* if(Serial.available()) {
+    value = Serial.parseInt();
+    Serial.println(value);
+  } */
+  delay(1000);  
 }
